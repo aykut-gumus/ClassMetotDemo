3acd85dc2e566d4e313c0e5636bab1efd6b39f2c
13616342556 <13616342556@C06TB030154>
2022-06-15 09:53:12 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Yeni müşteri : " + musteri.Name + " " + musteri.Surname + " eklendi.");
+        }
+
+        public void List(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Name + " " + musteri.Surname);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using ClassMetotDemo;
+
+Musteri musteri1 = new Musteri();
+musteri1.Id = 1;
+musteri1.Name = "Eren";
+musteri1.Surname = "Kara";
+
+Musteri musteri2 = new Musteri();
+musteri2.Id = 2;
+musteri2.Name = "Esin";
+musteri2.Surname = "Kibar";
+
+Musteri musteri3 = new Musteri();
+musteri3.Id = 3;
+musteri3.Name = "Halit";
+musteri3.Surname = "Güral";
+
+Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+MusteriManager musteriManager = new MusteriManager();
+
+musteriManager.Add(musteri1);
+musteriManager.List(musteriler);
+
